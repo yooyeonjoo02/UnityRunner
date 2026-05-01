eb84115303abe35a72aee367418cec4c103c4dae
[CLOUD4] 유연주 <ccomi0122@gmail.com>
2026-05-01 16:57:44 +0900

    feat: 게임 종료 후 기록 저장 및 랭킹 UI 구현

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,44 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Record
+{
+    public string name;
+    public float time;
+
+    public Record(string name, float time)
+    {
+        this.name = name;
+        this.time = time;
+    }
+}
 
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI timerText;
     public GameObject gameOverPanel;
+    public TMP_InputField nameInputField;
+    public TextMeshProUGUI currentRecordText;
+    public TextMeshProUGUI recordListText;
     public float gameSpeed = 10f;
 
     private float time;
     private bool isGameOver = false;
+
+    private List<Record> records = new List<Record>();
+
+    void Start()
+    {
+        Time.timeScale = 1f;
+
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(false);
+
+        ShowRecordList();
+    }
 
     void Update()
     {
@@ -26,6 +55,78 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 0f;
 
         gameOverPanel.SetActive(true);
+        currentRecordText.text = "Record: " + time.ToString("F1") + " sec";
+    }
+
+    public void SaveRecord()
+    {
+        string playerName = nameInputField.text;
+
+        if (playerName == "")
+            playerName = "Player";
+
+        LoadFromPrefs();
+
+        records.Add(new Record(playerName, time));
+
+        records.Sort((a, b) => b.time.CompareTo(a.time));
+
+        if (records.Count > 5)
+            records.RemoveAt(records.Count - 1);
+
+        SaveToPrefs();
+        ShowRecordList();
+    }
+
+    private void SaveToPrefs()
+    {
+        PlayerPrefs.SetInt("RecordCount", records.Count);
+
+        for (int i = 0; i < records.Count; i++)
+        {
+            PlayerPrefs.SetString("Name_" + i, records[i].name);
+            PlayerPrefs.SetFloat("Time_" + i, records[i].time);
+        }
+
+        PlayerPrefs.Save();
+    }
+
+    private void LoadFromPrefs()
+    {
+        records.Clear();
+
+        int count = PlayerPrefs.GetInt("RecordCount", 0);
+
+        for (int i = 0; i < count; i++)
+        {
+            string savedName = PlayerPrefs.GetString("Name_" + i, "Player");
+            float savedTime = PlayerPrefs.GetFloat("Time_" + i, 0f);
+
+            records.Add(new Record(savedName, savedTime));
+        }
+    }
+
+    private void ShowRecordList()
+    {
+        LoadFromPrefs();
+
+        string text = "Best Record\n";
+
+        if (records.Count == 0)
+        {
+            text += "No Record";
+        }
+        else
+        {
+            for (int i = 0; i < records.Count; i++)
+            {
+                text += (i + 1) + ". "
+                      + records[i].name + " - "
+                      + records[i].time.ToString("F1") + " sec\n";
+            }
+        }
+
+        recordListText.text = text;
     }
 
     public void RestartGame()
